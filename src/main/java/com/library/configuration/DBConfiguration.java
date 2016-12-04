8f960e2dbd6c5e01cf6601c6652b0dc90c14a1a0
sushneo <sush.neo@gmail.com>
2016-12-04 18:25:41 +0530

    DB resources driven through env variables

--- src/main/java/com/library/configuration/DBConfiguration.java
+++ src/main/java/com/library/configuration/DBConfiguration.java
@@ -5,25 +5,41 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
-import com.mongodb.DB;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoDatabase;
 
 @Configuration
 public class DBConfiguration {
 	
 	@Bean
 	@Scope(scopeName=ConfigurableBeanFactory.SCOPE_SINGLETON)
-	public DB getDB() {
-		MongoClient mongo = null;
-		DB db = null;
-		String uri ="mongodb://sushclient:client55@172.30.128.34:27017/?authSource=sampledb&authMechanism=SCRAM-SHA-1";
+	public MongoDatabase getDB() {
+		MongoClient mongoClient = null;
+		MongoDatabase database = null;
+		StringBuilder uriBuilder = new StringBuilder();
+		uriBuilder.append("mongodb://");
+		uriBuilder.append(System.getenv("MONGODB_USER"));
+		uriBuilder.append(":");
+		uriBuilder.append(System.getenv("MONGODB_PASSWORD"));
+		uriBuilder.append("@");
+		uriBuilder.append(System.getenv("MONGODB_PORT_27017_TCP_ADDR"));
+		uriBuilder.append(":");
+		uriBuilder.append(System.getenv("MONGODB_SERVICE_PORT"));
+		uriBuilder.append("/?authSource=");
+		uriBuilder.append(System.getenv("MONGODB_DATABASE"));
+		uriBuilder.append("&authMechanism=SCRAM-SHA-1");
+		
+		String uri = uriBuilder.toString();
+		//uri = "mongodb://sushclient:client55@localhost:27018/?authSource=sampledb&authMechanism=SCRAM-SHA-1";
+		System.out.println("********MongoDB URL::"+uri+" *********");
 		try{
-			mongo = new MongoClient(new MongoClientURI(uri));
+			mongoClient = new MongoClient(new MongoClientURI(uri));
 		}catch(Exception e) {
-			mongo.close();
+			mongoClient.close();
 		}
-		db = mongo.getDB("sampledb");
-		return db;
+		database = mongoClient.getDatabase(System.getenv("MONGODB_DATABASE"));
+		return database;
 	}
+	
 }
--- src/main/java/com/library/controller/LibraryController.java
+++ src/main/java/com/library/controller/LibraryController.java
@@ -3,6 +3,7 @@ package com.library.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,33 +12,32 @@ import org.springframework.web.bind.annotation.RestController;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.library.model.Books;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 @RestController
 public class LibraryController {
 	
 	@Autowired(required=false)
-	private DB db;
+	private MongoDatabase database;
 	
 	private final String producerType = "application/json";
 	
 	@RequestMapping(value="/books", method=RequestMethod.GET, produces={producerType})
 	public List<Books> getAllBooks() {
-		System.out.println("#############In LibraryController###############");
-		if(db != null){
-		DBCollection collection = db.getCollection("books");
-		DBCursor cursor = collection.find();
+		System.out.println("LibraryController::Processing book request");
+		if(database != null){
+		MongoCollection<Document> collection = database.getCollection("books");
+		List<Document> documents = collection.find().into(new ArrayList<Document>());
 		GsonBuilder gsonBuilder = new GsonBuilder();
 	    Gson gson = gsonBuilder.create();
 	    List<Books> booklist = new ArrayList<Books>();
 		try {
-			   while(cursor.hasNext()) {
-				   booklist.add(gson.fromJson(cursor.next().toString(), Books.class));
+			   for (Document document : documents) {
+				   booklist.add(gson.fromJson(document.toJson() , Books.class));
 			   }
-		} finally {
-			   cursor.close();
+		} catch (Exception e) {
+			System.out.println("Exception while parsing to GSON");
 		}
 		
 		return booklist;
